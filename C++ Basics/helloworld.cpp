fdcb25dfe1c409ac50311e4d56f6843177d4c01b
Russell Mehta <RussellSoham@gmail.com>
2024-08-06 09:36:34 +0700

    learned input mechnanism

--- C++ Basics/helloworld.cpp
+++ C++ Basics/helloworld.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+using namespace std; //not recommended but helpful for beginners
 
-int main() {
-    std::cout<<"Hello World!";
+int main() 
+{
+    //std::cout<<"Hello World"; --> since we use namespace, we don't need to type "std::" to save time
+    
+    cout<<"Hello World!";
 
     return 0;
 }
---
+++ C++ Basics/inputname.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str;
+
+    cout<<"May I know your name? ";
+    cin>>str;
+
+    cout<<"Sup "<<str;
+
+
+    //for full name
+/*     string fullStr;
+
+    cout<<"May I know your name? ";
+    
+    getline(cin, fullStr);
+
+    cout<<"Sup "<<fullStr; */
+
+
+    return 0;
+}
